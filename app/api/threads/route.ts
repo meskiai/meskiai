5096b93400905dcc572d0b0bfaaa8eb45eddf735
Miłosz Męski <miloszmeski@MacBook-Pro.local>
2026-07-27 03:14:37 +0200

    Fix UI: Prevent caching of dashboard threads

--- app/api/threads/route.ts
+++ app/api/threads/route.ts
@@ -3,6 +3,8 @@ import { getServerSession } from 'next-auth/next';
 import { authOptions } from '../auth/[...nextauth]/route';
 import { prisma } from '../../../lib/prisma';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: Request) {
   const session = await getServerSession(authOptions);
   
